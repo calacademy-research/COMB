e6f66f08ffd02cd34cac9934be130afbb113b886
Jack Dumbacher <jdumbacher@calacacdemy.org>
2022-03-24 10:19:44 -0700

    Worked on code that will filter aru2point dataframe to only include those recordings that are on a point

---
+++ acoustic/data_ingest/src/readML.subset.R
@@ -0,0 +1,59 @@
+#############################################################
+## readML.subset.R --
+## read in Tom's machine learning second results set March 2021!
+## Script to get in PARTIAL dataset of Google machine learning of birds
+## from outputs that have already been read in by readML.R
+##  and subsequently filtered.  In this particular case, it loads in 
+## the dataML_m1.5.csv file, but that can easily be changed by making
+## a substitution in the code.  The idea is to get a working copy of the 
+## ML data in so that other work can be done...  
+## Team Caples (Jack Dumbacher, Mary Clapp, Durrell Kapan, Mark Schulist)
+## built on readML.R from CAPLES/scripts/readML.R
+## ############################################################
+# Load libraries ------------------------------------------------------------
+
+# libraries required for this script
+library(tidyverse)
+library(data.table)
+library(here)
+library(googledrive)
+library(lubridate)
+library(fs)
+library(furrr)
+
+# Load some Caples functions
+
+source(here("comb_functions.R"))
+
+############# OPTIONS FOR SCRIPT ###################
+here <- here()
+
+dataML <- fread(paste0(here, "/acoustic/data_ingest/output/dataML_m1.5.csv"))
+
+
+# Add the survey point number where each recording took place ------------------------------------------------------------
+
+# Create symlink from output of file_to_point to input of readML.R
+if (file.exists(here("acoustic/data_ingest/input/aru2point.csv")) == F) {
+  drive_download(as_id("1e2DV2eykUskpffVqIZBOjmB08x3Z_cbt5NK93B3V6JE"), here("acoustic/data_ingest/input/aru2point.csv"))
+}
+
+# READ IN THE ARU to actual point key value pair file from the file: 2020_ARU_data.soundfilelist.csv
+aru2point <- fread(here("acoustic/data_ingest/input/aru2point.csv"), header = TRUE, sep = ",") %>%
+  separate(filename, into = c("File_ID", "filetype"), sep = "\\.", remove = FALSE) %>%
+  filter(!is.na(as.numeric(point))) %>%  # throws out all that are NA or have text and are not on a point
+  filter(as.numeric(point)>0)
+
+distinct(aru2point[,4])
+distinct(dataML[,7])
+dataML %>% distinct(Point)
+distinct(dataML,Point)
+
+dataML %>% 
+  arrange(distinct(Point))
+
+
+
+
+
+
