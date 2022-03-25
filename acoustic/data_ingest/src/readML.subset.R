57b616e2b9271f1c03944ab476e83bfc3d0aea8b
Jack Dumbacher <jdumbacher@gmail.com>
2022-03-25 14:49:16 -0700

    Added some code used to filter and summarize data in order to create the Y[ARUi,j] matrix input for Mary's model.

--- acoustic/data_ingest/src/readML.subset.R
+++ acoustic/data_ingest/src/readML.subset.R
@@ -63,6 +63,22 @@ dataML_m2.0 <- dataML %>% filter(rlang::eval_tidy(rlang::parse_expr(filter_value
 
 cols <- colnames(dataML)[1:100]
 
+#########################################
+# to create a subset of data with a given 
+# minimum threshold across all species
+##########################################
+cols <- colnames(dataML)[12:100]
+
+# For this example, the cutoff is max logit > -2
+
+filter_values <- paste(cols, ">-0.5", "| ") %>% 
+  paste(collapse = "") %>% 
+  str_sub(end = -4)
+
+dataML_m0.5 <- dataML %>% filter(rlang::eval_tidy(rlang::parse_expr(filter_values)))
+
+fwrite(dataML_m0.5, here("acoustic/data_ingest/output/dataML_m0.5.csv"))
+
 
 
 ###########################################################
@@ -91,6 +107,40 @@ p
 # Rotate the violin plot
 p + coord_flip()
 
+
+##############################################################
+# this section of code subsets the dataML dataframe
+# to provide raw data for Y[ARUi,j]
+# where i is the ith point
+# and j is the jth recording made on point i,
+# and sum is the total number of detections exceeding 
+# a particular threshold (in the case below, logit > 0.5)
+# 
+# we are additionally using only recordings taken from 
+# 06:00 to 10:00am, complete recordings, recordings 
+# made on a point.  
+##############################################################
+dataML_model <- dataML %>% 
+  filter(!is.na(as.numeric(point))) %>%  # throws out all that are NA or have text and are not on a point
+  filter(as.numeric(point)>0) %>% 
+  select(filename, File_ID, ARU_ID, Date_Time, point, Start_Time, rebnut) %>% 
+  mutate(P = logit_to_p(rebnut)) %>% 
+  filter(year(Date_Time) == 2021)
+
+fwrite(dataML_model, here("acoustic/data_ingest/output/dataML_model.csv"))
+
+dataML_visit <- dataML_model %>% 
+  group_by(File_ID, point, Date_Time) %>% 
+  summarize(sum=sum(rebnut>1.0)) %>% 
+  filter(hour(Date_Time)>=6, hour(Date_Time)<10) %>% 
+  filter(minute(Date_Time)==00 | minute(Date_Time)==30)
+
+dataML_point <- dataML_visit %>% 
+  group_by(point) %>% 
+  summarize(n=sum(sum>0))
+
+ggplot(data=dataML_point, aes(x=n)) +
+  geom_histogram(binwidth = 5)
 
 
 
