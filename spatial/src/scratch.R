d9c28da23151e6749785abe0fc2ff66c38e97709
durrell <ddkapan@yahoo.com>
2022-04-18 18:18:51 -0700

    changed minor scratch code /spatial/src/scratch.R

--- spatial/src/scratch.R
+++ spatial/src/scratch.R
@@ -1,3 +1,17 @@
+# Detach all  packages
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search())) == 1, TRUE, FALSE)]
+
+  package.list <- setdiff(package.list, basic.packages)
+
+  if (length(package.list) > 0) for (package in package.list) detach(package, character.only = TRUE)
+}
+
+
+
+
 # random non-cleaned up scratch work ... ignore and don't leave good stuff here
 
 data.rot.melt <- reshape2::melt(data.pca$rotation[, 1:5]) # Melt matrix (needed for ggplot), factors 1-5
