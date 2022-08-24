fa4fffe22f63444f569a632e17fc4f23bc379021
Matt Harvey <mattharvey@google.com>
2022-08-24 11:36:04 -0700

    Truncates GMM components to > 0.5

--- models/src/combined.R
+++ models/src/combined.R
@@ -58,32 +58,37 @@ model {
   ome ~ dunif(0, 1000) # omega: rate of non-target detections
 
   # Parameters of the observation model for the scores
-  mu[1] ~ dnorm(0, 0.01)
-  mu[2] ~ dnorm(0, 0.01)
-  sigma ~ dunif(0, 10)
-  tau <- 1 / (sigma * sigma)
+  mu[1] ~ dnorm(-1.3, 1)T(0.5,)     # -1.3, sig 1
+  mu[2] ~ dnorm(-1.75, 0.1)T(0.5,)    # -1.75, sig 0.125
+  sigma[1] ~ dunif(0, 10)
+  tau[1] <- 1 / (sigma[1] * sigma[1])
+  sigma[2] ~ dunif(0, 10)
+  tau[2] <- 1 / (sigma[2] * sigma[2])
 
   # Likelihood part 1: detection data and ARU counts
   for (i in 1:nsites) { # Loop over sites
     z[i] ~ dbern(psi[i]) # Latent occupancy states
-    
+
     p[i] <- z[i]*p11 + (1-z[i])*p10 # Detection probability including over-detections
-    
+
     for(j in 1:nsurveys.pc) { # Loop over occasions
       y.ind[i,j] ~ dbern(p[i]) # Observed occ. data (if available)
     }
-    
+
     site.prob[i] <- lam*z[i]/(lam*z[i]+ome) # Pr(sample is target species) ... probability that a logit score came from a true detection
-    
+
     for(j in 1:nsurveys.aru) { # Loop over occasions
       y.aru[i,j] ~ dpois(lam*z[i] + ome)  # Total samples processed
     }
   }
 
   # Likelihood part 2: feature score data
+  for (i in 1:nsites) {
+    site.prob[i] <- lam*z[i]/(lam*z[i]+ome) # Pr(sample is target species)
+  }
   for(k in 1:nsamples) {
     # Sample specific covariate
-    score[k] ~ dnorm(mu[g[k]], tau) # parameters are group specific
+    score[k] ~ dnorm(mu[g[k]], tau[g[k]]) # parameters are group specific
     probs[k,1] <- site.prob[siteid[k]]
     probs[k,2] <- 1 - site.prob[siteid[k]] # the prior class probabilities
     g[k] ~ dcat(probs[k,])
@@ -103,7 +108,7 @@ gst[data$score > threshold] <- 1
 gst[data$score <= threshold] <- 2
 inits <- function() {
   list(
-    mu = c(1, -1), sigma = 0.2, z = zst,
+    mu = c(1, 0.6), sigma = c(1, 0.1), z = zst,
     psi = runif(1), p10 = runif(1, 0, 0.05), p11 = runif(1, 0.5, 0.8),
     lam = runif(1, 1, 2), ome = runif(1, 0, 0.4), g = gst
   )
@@ -116,24 +121,19 @@ monitored <- c("psi", "p10", "p11", "lam", "ome", "mu", "sigma", "Npos", "z")
 
 # MCMC settings
 na <- 1000
-ni <- 2000
+ni <- 4000
 nt <- 1
 nb <- 1000
-nc <- 3
+nc <- 6
 
 # TODO(matth79): JAGS does not like indices in the list, since it's non-numeric.
 # Discuss team preferences on whether to omit it, nest the return value of
 # readCombined, or some other alternative.
 jagsData <- within(data, rm(indices))
 
+set.seed(123)
+
 jagsResult <- jags(jagsData, inits, monitored, modelFile,
   n.adapt = na,
   n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, parallel = TRUE,
-  seed = 123
 )
-plot(jagsResult)
-
-
-# EDA
-# "naive occupancy probability" from point count data only
-sum(data$y.ind[rowSums(data$y.ind, na.rm=TRUE) != 0])/84
