8876a925a12923a7b433f68eb61ebfd5bcbb9d75
mschulist <mschulist2@gmail.com>
2022-12-15 17:20:20 -0800

    started making simulation

---
+++ models/src/simulation.R
@@ -0,0 +1,72 @@
+## simulation.R
+## Script to simulate data to 'test' the model
+
+library(tidyverse)
+library(truncnorm)
+source("comb_functions.R")
+
+#' simulation
+#' @param species Species Code that we will simulate data for
+#' @param psi Theoretical probability of occupancy for the given species
+#' @param n_visits Number of visits by human point counters
+#' @param n_points Number of points
+#' @param n_recordings Number of recordings at each point by an ARU
+#' @param mu Vector containing the means for the two Gaussian distributions of
+#' the scores. The first is the negative mu and the second is the positive. 
+#' @param sigma Vector containing the standard deviations of the two Gaussian
+#' distributions of the scores. The first is the negative mu and the second is 
+#' the positive. 
+#' @return List similar to that of `model_read_lib` that JAGS can read
+
+simulation <- function(species, psi, p11, p_aru11, p_aru01, n_visits = 3, n_points = 82, 
+                       n_recordings = 24, mu, sigma){
+  ## Point Count Simulation ------------------------------------------------------
+  # Generate some Bernoulli trials
+  set.seed(123)
+  
+  p <- psi * p11
+  
+  y.ind <- rbinom(n_visits * n_points, size = 1, prob = p) %>% 
+    matrix(nrow = n_points, ncol = n_visits)
+  
+  ## ARU Simulation --------------------------------------------------------------
+  
+  # We will first define the `y.aru` which is a matrix similar to y.ind
+  # and has binary data on if the ARU has a detection of the species above an
+  # arbitrary logit
+  nsamples <- n_recordings * n_points
+  
+  p_aru <- psi * p_aru11 + p_aru01
+  
+  y.aru <- rbinom(nsamples, size = 1, prob = psi) %>% 
+    matrix(nrow = n_points, ncol = n_recordings)
+  
+  # Next we will simulate the scores
+  
+  # The scores are distributed as two truncated Gaussian distributions
+  # with parameters mu and sigma. 
+  # The 'truncation' will be a function of sigma in relation to mu
+  
+  negatives <- rtruncnorm(nsamples * (1 - psi), mean = mu[1], sd = sigma[1], a = -3, b = 10)
+  positives <- rtruncnorm(nsamples * psi, mean = mu[2], sd = sigma[2], a = -3, b = 10)
+  
+  # making the `scores` vector with random order
+  scores <- c(negatives, positives) %>% sample(length(c(negatives, positives)))
+  
+  # Combining for JAGS -----------------------------------------------------------
+  
+  data <- list(
+    "nsites" = n_points,
+    "nsurveys.pc" = n_visits,
+    "nsurveys.aru" = n_recordings,
+    "y.ind" = y.ind,
+    "y.aru" = y.aru,
+    "nsamples" = length(scores),
+    "siteid" = rep_len(1:n_points, length(scores)),
+    "score" = scores
+  )
+  data
+}
+
+data <- simulation("GCKI", n_points = 80, psi = 0.62, p11 = 0.6, p_aru11 = 0.7,
+                   p_aru01 = 0.05, mu = c(-1.5, 1.3), sigma = c(0.8, 2.3))
