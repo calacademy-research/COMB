cc2739c4349fb3c8e17e3e86a0012995c3d84cf4
Mary Clapp <mkclapp@ucdavis.edu>
2022-12-23 14:36:04 -0800

    dec bug fix

--- models/src/max_pc_scores_arubin_2covars.R
+++ models/src/max_pc_scores_arubin_2covars.R
@@ -17,7 +17,7 @@ source(here("models/src/model_read_lib_agg.R")) # Functions were modified to rea
 
 
 # parameters --------------------------------------------------------------
-speciesCode <- "WEWP" # must match prefiltering of dataML_model.csv
+speciesCode <- "YRWA" # must match prefiltering of dataML_model.csv
 year <- 2021
 threshold <- 0
 aruVisitLimit <- 24 # only consider this many ARU visits per site (ordered)
@@ -36,9 +36,9 @@ data <- readCombined(
   squeeze = T,
   logit_col = "max_logit" # This is specifying we want the max_logit column from aggregated data
 )
-data$y.pc[,1,,] # performs a quick check to see if pointcount data were read in properly
-data$y.aru # aru detections
-data$score
+data$y.pc[,1,,] # quick look at point count data
+data$y.aru # aru detection data
+data$score # score data
 
 # JAGS specification ------------------------------------------------------
 modelFile <- tempfile()
@@ -51,7 +51,9 @@ model {
   p11 ~ dbeta(2, 2) # p11 = Pr(y = 1 | z = 1)
   p_aru11 ~ dbeta(2, 2) # p11 = Pr(y = 1 | z = 1)
   p_aru01 ~ dbeta(1, 3)I(0, 1 - p_aru11) # p11 = Pr(y = 1 | z = 0)
-  beta0 ~ dnorm(0, 10) # Intercept for occupancy logistic regression
+  #beta0 ~ dnorm(0, 10) # Intercept for occupancy logistic regression
+  mean.psi ~ dunif(0, 1) 
+  beta0 <- logit(mean.psi)
   beta1 ~ dnorm(0, 10) 
   beta2 ~ dnorm(0, 10) 
 
@@ -120,16 +122,68 @@ model {
   czSim0 <- sum(1 - zSim)
   TvegSim <- sum(cover*zSim) / ifelse(czSim1>0,czSim1, 1) - sum(cover*(1-zSim))/ifelse(czSim0>0,czSim0, 1)
 
-  mean_psi <- mean(psi)
   NOcc <- sum(z[]) # derived quantity for # of sites occupied (to compare with 'naive' sum(y.aru[]) and sum(y.pc[])
   PropOcc <- NOcc/nsites
 
 }
 ")
 
+
+# Add GoF inputs to data list -----------------------------------------------
+
+n_v_per_site <-
+  rowSums(!is.na(data$y.ind[, 1:3])) # number of visits
+y_pc_sum <- rowSums(data$y.ind[, 1:3], na.rm = TRUE)
+n_samp_per_site <-
+  rowSums(!is.na(data$y.aru[, 1:24])) # number of samples
+y_aru_sum <- rowSums(data$y.aru[, 1:24], na.rm = TRUE) # number of samples where y_aru=1
+
+
+data2 <-
+  append(
+    data,
+    list(
+      n_v = n_v_per_site,
+      y_pc_sum = y_pc_sum,
+      n_s = n_samp_per_site,
+      y_aru_sum = y_aru_sum
+    )
+  )
+
+
+# TODO(matth79): JAGS does not like indices in the list, since it's non-numeric.
+# Discuss team preferences on whether to omit it, nest the return value of
+# readCombined, or some other alternative.
+jagsData <- within(data2, rm(indices))
+
+
+# Add site-level covariates to data list ----------------------------------
+
+# TODO: tighten up this code; someday automatically do this with model_read_lib or similar script? 
+# TODO: ...and also include visit-level covariates?
+
+site_covars <- read_csv("./models/input/wide4havars.csv") %>%
+  mutate(Point = avian_point) %>%
+  filter(Point %in% data$indices$point$Point)
+
+colnames(site_covars) # list of options for site-level covariates-- I chose measures of canopy cover and burn severity at the 4ha scale
+
+site_covars <- read_csv("./models/input/wide4havars.csv") %>%
+  mutate(Point = avian_point) %>%
+  filter(Point %in% data$indices$point$Point) # filter to only points with avian data
+
+colnames(site_covars) # list of options for site-level covariates to choose from-- with select() below I chose measures of canopy cover and burn severity at the 4ha scale
+covs <- site_covars %>% dplyr::select(Point, mean_CanopyCover_2020_4ha, mean_RAVGcbi_20182019_4ha) %>% arrange(Point)
+
+Veg <-left_join(as.data.frame(data$indices$point), covs, by = "Point") # ensures veg vars are in the same point order as the count data
+
+jagsData$cover <- as.numeric(scale(Veg$mean_CanopyCover_2020_4ha))
+jagsData$burn <- as.numeric(scale(Veg$mean_RAVGcbi_20182019_4ha)) # TODO: should we scale this or no?
+
+
+# Set monitored parameters and starting values ----------------------------
 # initialization
 zst <- rep(1, data$nsites)
-psit = runif(data$nsamples)
 gst <- sample(1:2, data$nsamples, replace = TRUE)
 gst[data$score > 0.0] <- 1
 gst[data$score <= 0.0] <- 2
@@ -138,30 +192,22 @@ inits <- function() {
     mu = c(-2, 0),
     sigma = c(1, 1),
     z = zst,
-    # psi = psit,
+    mean.psi = runif(1),
     p11 = runif(1, 0.2, 0.8),
-    #  lambda = runif(1, 1, 20), omega = runif(1, 0, 15),
     g = gst,
-    beta0 = 0,
-    beta1 = 0,
-    beta2=0,
     reg_parm = 1
   )
 }
-
-
-# JAGS execution ----------------------------------------------------------
-
 monitored <- c(
+  "mean.psi",
   "beta0",
   "beta1",
   "beta2",
   "p11",
   "p_aru11",
   "p_aru01",
-  "mu",
-  "sigma",
-  "mean_psi",
+  "mu", # score distributions 
+  "sigma", # sd for score distributions
   "NOcc", "PropOcc",
   "T_pc_obs",
   "T_pc_sim",
@@ -180,43 +226,9 @@ nt <- 1
 nb <- 1000
 nc <- 6
 
+# JAGS execution ----------------------------------------------------------
 
-n_v_per_site <-
-  rowSums(!is.na(data$y.ind[, 1:3])) # number of visits
-y_pc_sum <- rowSums(data$y.ind[, 1:3], na.rm = TRUE)
-n_samp_per_site <-
-  rowSums(!is.na(data$y.aru[, 1:24])) # number of samples
-y_aru_sum <- rowSums(data$y.aru[, 1:24], na.rm = TRUE)
-
-
-data2 <-
-  append(
-    data,
-    list(
-      n_v = n_v_per_site,
-      y_pc_sum = y_pc_sum,
-      n_s = n_samp_per_site,
-      y_aru_sum = y_aru_sum
-    )
-  )
-# TODO(matth79): JAGS does not like indices in the list, since it's non-numeric.
-# Discuss team preferences on whether to omit it, nest the return value of
-# readCombined, or some other alternative.
-jagsData <- within(data2, rm(indices))
-
-site_covars <- read_csv("./models/input/wide4havars.csv") %>%
-  mutate(Point = avian_point) %>%
-  filter(Point %in% data$indices$point$Point)
-
-colnames(site_covars) # list of options for site-level covariates-- I chose measures of canopy cover and burn severity at the 4ha scale
-
-covs <- site_covars %>% dplyr::select(Point, mean_CanopyCover_2020_4ha, mean_RAVGcbi_20202021_4ha)
-
-Veg <-left_join(as.data.frame(data$indices$point), covs, by = "Point")
-jagsData$cover <- as.numeric(scale(Veg$mean_CanopyCover_2020_4ha))
-jagsData$burn <- as.numeric(Veg$mean_RAVGcbi_20202021_4ha) # check out a histogram of this... sort of bimodal 
-
-set.seed(123)
+#set.seed(123)
 
 jagsResult <- jags(
   jagsData,
@@ -230,6 +242,20 @@ jagsResult <- jags(
   n.burnin = nb,
   parallel = TRUE,
 )
+
+## Quick QC checks
+print(jagsResult)
+# table(apply(data$y.pc[, 1, , ], 1, max), apply(data$y.aru, 1, max)) # number of sites with detections by ARU versus point count
+# (site_pos <-
+#     cbind(apply(data$y.pc[, 1, , ], 1, max), apply(data$y.aru, 1, max)))
+# mean(apply(site_pos, 1, max), na.rm = TRUE) # average number of sites with positive detections from either source
+# plogis(jagsResult$mean$beta0) # Mean of the posterior of the mean occupancy rate psi across sites from model
+# jagsResult$mean$mean.psi
+# 
+# mean(apply(data$y.pc[, 1, , ], 1, max), na.rm = TRUE)
+# mean(apply(data$y.aru, 1, max), na.rm = TRUE)
+
+# Posterior Predictive Checks ---------------------------------------------
 
 pp.check(
   jagsResult,
@@ -291,14 +317,3 @@ pp.check(
       sep = " "
     )
 )
-
-## Result QC checks
-print(jagsResult)
-table(apply(data$y.pc[, 1, , ], 1, max), apply(data$y.aru, 1, max)) # comparing sites with detections by ARU versus point count
-(site_pos <-
-  cbind(apply(data$y.pc[, 1, , ], 1, max), apply(data$y.aru, 1, max)))
-mean(apply(site_pos, 1, max), na.rm = TRUE) # average number of sites with positive detections from either source
-jagsResult$mean$mean_psi # Mean of the posterior of the mean occupancy rate psi across sites from model
-
-mean(apply(data$y.pc[, 1, , ], 1, max), na.rm = TRUE)
-mean(apply(data$y.aru, 1, max), na.rm = TRUE)
