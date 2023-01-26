a330df17f9326ed92ff6e3f22769b6154bfecef4
mschulist <mschulist2@gmail.com>
2023-01-26 15:10:53 -0800

    started new simulation

--- models/src/simulation.R
+++ models/src/simulation.R
@@ -12,73 +12,73 @@ source("comb_functions.R")
 #' @param n_points Number of points
 #' @param n_recordings Number of recordings at each point by an ARU
 #' @param mu Vector containing the means for the two Gaussian distributions of
-#' the scores. The first is the negative mu and the second is the positive. 
+#' the scores. The first is the negative mu and the second is the positive.
 #' @param sigma Vector containing the standard deviations of the two Gaussian
-#' distributions of the scores. The first is the negative mu and the second is 
-#' the positive. 
+#' distributions of the scores. The first is the negative mu and the second is
+#' the positive.
 #' @return List similar to that of `model_read_lib` that JAGS can read
 
-simulation <- function(species, psi, p11, p_aru11, p_aru01, n_visits = 3, 
-                       n_points = 82, n_recordings = 24, mu, sigma){
+simulation <- function(species, psi, p11, p_aru11, p_aru01, n_visits = 3,
+                       n_points = 82, n_recordings = 24, mu, sigma) {
   ## Point Count Simulation ----------------------------------------------------
   # Generate some Bernoulli trials
   set.seed(123)
-  
+
   z <- rbinom(n_points, 1, psi) # occupancy states
-  
-  #p <- psi * p11 # detection probability
-  
+
+  # p <- psi * p11 # detection probability
+
   y.ind <- matrix(NA, nrow = n_points, ncol = n_visits)
-    
-  for(i in 1:n_points) {
-    pr_yequals1 <- p11*z[i]
+
+  for (i in 1:n_points) {
+    pr_yequals1 <- p11 * z[i]
     y.ind[i, ] <- rbinom(n_visits, 1, pr_yequals1)
   }
-  
-    # rbinom(n_visits * n_points, size = 1, prob = p) %>% 
-    # matrix(nrow = n_points, ncol = n_visits)
-  
+
+  # rbinom(n_visits * n_points, size = 1, prob = p) %>%
+  # matrix(nrow = n_points, ncol = n_visits)
+
   ## ARU Simulation ------------------------------------------------------------
-  
+
   # We will first define the `y.aru` which is a matrix similar to y.ind
   # and has binary data on if the ARU has a detection of the species above an
   # arbitrary logit
   nsamples <- n_recordings * n_points
-  
+
   y.aru <- matrix(NA, nrow = n_points, ncol = n_recordings)
-  
-  for(i in 1:n_points) {
+
+  for (i in 1:n_points) {
     p_aru <- z[i] * p_aru11 + p_aru01
     y.aru[i, ] <- rbinom(n_recordings, 1, p_aru)
   }
-  
-  
-  # y.aru <- rbinom(nsamples, size = 1, prob = p_aru) %>% 
-  #   matrix(nrow = n_points, ncol = n_recordings)
-  
-  # Next we will simulate the scores
-  
-  # The scores are distributed as two truncated Gaussian distributions
-  # with parameters mu and sigma. 
-  # The 'truncation' will be a function of sigma in relation to mu
-  
-  # negatives <- rtruncnorm(nsamples * (1 - psi), mean = mu[1], sd = sigma[1], a = -3, b = 10)
-  # positives <- rtruncnorm(nsamples * psi, mean = mu[2], sd = sigma[2], a = -1, b = 10)
-  
-  positives <- rnorm(nsamples * psi, mean = mu[2], sd = sigma[2])
-  negatives <- rnorm(nsamples * (1 - psi), mean = mu[1], sd = sigma[1])
 
-  positive_pts <- which(z == 1)
-  negative_pts <- which(z == 0)
-  
-  siteid_pos <- sample(positive_pts, size = nsamples * psi, replace = T)
-  siteid_neg <- sample(negative_pts, size = nsamples * (1 - psi), replace = T)
-  
-  # making the `scores` vector with random order
-  scores <- c(negatives, positives)
-  
+
+  # Next we will simulate the scores
+
+  # The scores are distributed as two truncated Gaussian distributions
+  # with parameters mu and sigma.
+
+  scores <- c()
+
+  # for each of the positive pts, simulate a vector of scores with len=n_recordings
+  for (i in 1:n_points) {
+    if (n_recordings != 0) {
+      for (j in 1:n_recordings) {
+        if (z[i] == 1) {
+          scores[i * j] <- rnorm(1, mean = mu[2], sd = sigma[2])
+        }
+        if (z[i] == 0) {
+          scores[i * j] <- rnorm(1, mean = mu[1], sd = sigma[1])
+        }
+      }
+    }
+  }
+
+  sites <- rep(1:n_points, each = n_recordings)
+
+
   # Combining for JAGS ---------------------------------------------------------
-  
+
   data <- list(
     "nsites" = n_points,
     "nsurveys.pc" = n_visits,
@@ -86,12 +86,12 @@ simulation <- function(species, psi, p11, p_aru11, p_aru01, n_visits = 3,
     "y.ind" = y.ind,
     "y.aru" = y.aru,
     "nsamples" = length(scores),
-    "siteid" = c(siteid_neg, siteid_pos),
+    "siteid" = sites,
     "score" = scores
   )
   data
 }
 
 # data <- simulation("GCKI", n_points = 80, psi = 0.62, p11 = 0.6, p_aru11 = 0.7,
-#                    p_aru01 = 0.05, mu = c(-2, 1.5), sigma = c(0.8, 2.3), 
+#                    p_aru01 = 0.05, mu = c(-2, 1.5), sigma = c(0.8, 2.3),
 #                    n_visits = 1, n_recordings = 1)
