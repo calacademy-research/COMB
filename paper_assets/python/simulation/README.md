4bea0422dc7c9041c4e4890de98cfe61abad1885
Mark Schulist <mschulist2@gmail.com>
2025-07-19 08:28:09 -0700

    fix hashing, add readme

--- paper_assets/python/simulation/README.md
+++ paper_assets/python/simulation/README.md
@@ -1,1 +1,29 @@
 # Simulation code for COMB
+
+This code allows us to run simulations for the COMB paper, and keep track of the results in a systematic way.
+
+The main idea is that we have many different combinations of parameters to try, but these large number of combinations can end up becoming unwieldy to deal with. This library is designed to streamline the process of running these simulations and storing the outputs for further analysis.
+
+## Running simulations
+
+### Prerequisites
+
+JAGS must be installed, along with [uv](https://github.com/astral-sh/uv).
+
+### Configs
+
+The configs are located in `sim_lib/sim_configs.py`. For each parameter, add the different values you would like that parameter to take on. Be mindful that adding too many values quickly leads to a large number of combinations.
+
+### Running
+
+To run, use the following command
+
+```bash
+make run CONFIG=<config-name> NUM_SIMS=<nsims> NUM_PROCESSES=<nproc>
+```
+
+`NUM_SIMS` corresponds to the number of simulations you want to run for a single combination. `NUM_PROCESSES` corresponds to the number of processes you want to use when running (they will run concurrently).
+
+## Outputs
+
+The outputs will be located in `data/<config-name>`. Each combination gets assigned a hash, which allows you to rerun the same config and just add more simulations without removing old simulations.
--- paper_assets/python/simulation/sim_lib/sim_data.py
+++ paper_assets/python/simulation/sim_lib/sim_data.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Tuple
 import numpy as np
+import hashlib
 import json
 
 
@@ -80,109 +81,8 @@ class SimParams:
         """
         Hashes the parameters using a frozen set of the parameters
         """
-        return hash(json.dumps(self.__dict__, sort_keys=True, default=str))
+        h = hashlib.md5(
+            (json.dumps(self.__dict__, sort_keys=True, default=str)).encode()
+        ).digest()
 
-
-# class SimData:
-#     """
-#     SimData class to generate simulated data
-#     """
-
-#     y_pc: np.ndarray = np.ndarray([])
-#     y_aru: np.ndarray = np.ndarray([])
-#     scores: np.ndarray = np.ndarray([])
-
-#     def __init__(self, params: SimParams):
-#         # params holds all of the simulation parameters
-#         self.params = params
-
-#         # below are the parameters that will be used when running the model
-#         self.nsites = params.nsites
-#         self.nsurveys_aru = params.nsurveys_aru
-#         self.nsurveys_scores = params.nsurveys_aru
-#         self.nsurveys_pc = params.nsurveys_pc
-#         self.covar = params.covar
-#         self.siteid = params.siteid
-
-#     def gen_simulated_data(self):
-#         # using covariates, we generate latent occupancy states
-#         z = self._gen_latent_occupancy()
-#         # generate PC data
-#         self.y_pc = self._gen_pc_data(z)
-#         self.y_aru = self._gen_aru_data(z)
-#         self.scores = self._gen_aru_scores(self.y_aru)
-
-#     def _gen_latent_occupancy(self):
-#         """
-#         Generate latent occupancy states
-#         """
-#         psi_c = expit(self.params.beta0 + self.params.beta1 * self.params.covar)
-#         z = np.random.binomial(1, psi_c, self.params.nsites)
-#         return z
-
-#     def _gen_pc_data(self, z: np.ndarray) -> np.ndarray:
-#         """
-#         Generate point count data
-
-#         Args:
-#             z (numpy.ndarray): The latent occupancy states.
-
-#         Returns:
-#             y_pc (numpy.ndarray): The point count data.
-#         """
-#         y_pc = np.zeros((self.params.nsites, self.params.nsurveys_pc))
-
-#         for i in range(self.params.nsites):
-#             prob_y_eq_1 = self.params.p11 * z[i]
-#             y_pc[i] = np.random.binomial(1, prob_y_eq_1, self.params.nsurveys_pc)
-
-#         return y_pc
-
-#     def _gen_aru_data(self, z: np.ndarray) -> np.ndarray:
-#         """
-#         Generate ARU data
-
-#         Args:
-#             z (numpy.ndarray): The latent occupancy states.
-
-#         Returns:
-#             y_aru (numpy.ndarray): The ARU data.
-#         """
-#         y_aru = np.zeros((self.params.nsites, self.params.nsurveys_aru))
-
-#         for i in range(self.params.nsites):
-#             p_aru = z[i] * self.params.p_aru11 + self.params.p_aru01
-#             y_aru[i] = np.random.binomial(1, p_aru, self.params.nsurveys_aru)
-
-#         return y_aru
-
-#     def _gen_aru_scores(self, y_aru: np.ndarray) -> np.ndarray:
-#         """
-#         Generate ARU scores
-
-#         Args:
-#             y_aru (numpy.ndarray): The binary ARU data.
-
-#         Returns:
-#             y_aru_scores (numpy.ndarray): The ARU scores.
-#         """
-
-#         scores = np.zeros((self.params.nsites, self.params.nsurveys_aru))
-#         for i in range(self.params.nsites):
-#             for j in range(self.params.nsurveys_aru):
-#                 if y_aru[i, j] == 0:
-#                     min = -10
-#                     max = self.params.threshold
-#                     scores[i, j] = stats.truncnorm.rvs(
-#                         min, max, loc=self.params.mu[0], scale=self.params.sigma[0]
-#                     )
-#                 elif y_aru[i, j] == 1:
-#                     min = self.params.threshold
-#                     max = 10
-#                     scores[i, j] = stats.truncnorm.rvs(
-#                         min, max, loc=self.params.mu[1], scale=self.params.sigma[1]
-#                     )
-#                 else:
-#                     raise ValueError("Invalid y_aru value, must be 0 or 1")
-
-#         return scores
+        return int.from_bytes(h[:8], "big")
